cd505a2618b3917c4a2c3de3ba152dfab7c9fb4d
vijay khatri <vijaykhatri22@gmail.com>
2018-02-27 09:38:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function add(a,b){
 function subtract(a,b){
   return (a-b)
 }
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return (a/b)
+}
+function inc(n){
+  return n++
+}
+function dec(n){
+  return n--;
+}
